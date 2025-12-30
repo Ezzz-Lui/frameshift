b24fe8a690409b8be88438dd58b157febebd0c52
Ezzz-Lui <ogn.lui@gmail.com>
2025-12-30 07:37:20 -0600

    feat: add GridChallenges component to ChallengesContent

--- app/challenges/page.tsx
+++ app/challenges/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import GridChallenges from "@/components/challenges/grid-challenges";
 import SearchSection from "@/components/challenges/search-section";
 
 export default function ChallengesContent() {
   return (
     <div>
       <SearchSection />
+      <GridChallenges />
     </div>
   );
 }
