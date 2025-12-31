f810cbe600ca4e514e532384bd9569eec8b2636b
Ezzz-Lui <ogn.lui@gmail.com>
2025-12-31 04:08:19 -0600

    feat: enhance SidebarChallenge component with improved layout and styling

--- components/challenges/challenge-sidebar.tsx
+++ components/challenges/challenge-sidebar.tsx
@@ -1,12 +1,25 @@
 import type { Challenge } from "@/data/challenges/challenges-showcase";
-import { Card, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+import { Button } from "../ui/button";
 
 export default function SidebarChallenge(challenge: Challenge) {
   return (
     <Card className="p-4">
-        <CardHeader>
-            <CardTitle>Info</CardTitle>
-        </CardHeader>
+      <CardHeader>
+        <CardTitle className="font-extrabold text-xl">Challenge Info</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div>
+          <Label className="mb-2">Repo URL</Label>
+          <Input readOnly value={challenge.repoURL}></Input>
+        </div>
+        {/* <div className="mt-4">
+          <Label className="mb-2">Resources</Label>
+          <Button variant="link"></Button>
+        </div> */}
+      </CardContent>
     </Card>
   );
 }
