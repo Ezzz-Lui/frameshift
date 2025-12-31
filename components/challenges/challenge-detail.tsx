ac1ce7ccea528d03b6174a831cf3e2067f4c0029
Ezzz-Lui <ogn.lui@gmail.com>
2025-12-31 03:43:08 -0600

    feat: restructure ChallengeDetailItem layout to enhance readability and display challenge details

--- components/challenges/challenge-detail.tsx
+++ components/challenges/challenge-detail.tsx
@@ -1,15 +1,41 @@
 import type { Challenge } from "@/data/challenges/challenges-showcase";
-import { Card, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { FrameworkBadge, DifficultyBadge, TypeBadge } from "./badges-usage";
+import { Badge } from "../ui/badge";
 
 export default function ChallengeDetailItem(challenge: Challenge) {
   return (
     <Card>
       <CardHeader>
+        <CardDescription className="flex gap-2 capitalize mb-2">
+          <FrameworkBadge value={challenge.framework} />
+          <DifficultyBadge value={challenge.difficulty} />
+          <TypeBadge value={challenge.type} />
+        </CardDescription>
         <CardTitle className="font-extrabold text-xl">
-            {challenge.title}
+          {challenge.title}
         </CardTitle>
-        <CardDescription>{challenge.description}</CardDescription>
+        <CardDescription>Estimated Time: {challenge.time}</CardDescription>
       </CardHeader>
+      <CardContent className="text-muted-foreground">
+        {challenge.description}
+      </CardContent>
+      <CardFooter>
+        <div className="flex gap-2">
+          {challenge.tags.map((tag) => (
+            <Badge key={tag} variant="secondary">
+              {tag}
+            </Badge>
+          ))}
+        </div>
+      </CardFooter>
     </Card>
   );
 }
