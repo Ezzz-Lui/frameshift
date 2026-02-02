ef107413137cc8177d4f3d9652d2db30cbb489bf
Ezzz-Lui <ogn.lui@gmail.com>
2026-02-02 02:11:27 -0600

    refactor(styles): streamline CSS variables and update font imports
    - Remove unused font imports and consolidate CSS variable definitions for improved clarity and maintainability.
    - Adjust color variables for better consistency across themes, ensuring a more cohesive design.
    - Comment out legacy imports to facilitate future updates without losing context.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,30 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+// import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+import { Figtree } from "next/font/google";
+
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
 import { Nav } from "@/components/shared/nav";
 import FooterApp from "@/components/shared/footer-app";
 import { ViewTransition } from "react";
 
-const jetbrainsMono = JetBrains_Mono({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+// const jetbrainsMono = JetBrains_Mono({
+//   subsets: ["latin"],
+//   variable: "--font-sans",
+// });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+const fontSans = Figtree({subsets:['latin'],variable:'--font-sans'});
+
 
 export const metadata: Metadata = {
   title: "Frameshift - Tech challenges for every framework",
@@ -32,11 +37,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={jetbrainsMono.variable} suppressHydrationWarning>
+    <html lang="en" className={fontSans.variable} suppressHydrationWarning>
       <body
         className={`
-        ${geistSans.variable}
-        ${geistMono.variable}
         antialiased
         min-h-screen
         flex
